2a1fdcf03e3fb94e3f9e48fa33035d91cbfc7c6d
AlbertKhomich <khomich1022@gmail.com>
2023-10-13 14:45:28 +0200

    Section 4

--- routes.js
+++ routes.js
@@ -36,7 +36,7 @@ const requestHandler = function (req, res) {
   <html>
     <body>
     
-        <h1>My First Heading</h1>
+        <h1>My First Heading!</h1>
         <p>My first paragraph.</p>
                         
     </body>

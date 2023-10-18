3c88abf40f475319ab15f57c250faa30ccca6e07
AlbertKhomich <khomich1022@gmail.com>
2023-10-18 14:49:09 +0200

    handlebars

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParcer = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const rootDir = require('./util/path');
 const adminData = require('./routes/admin');
@@ -9,7 +10,16 @@ const shopRoutes = require('./routes/shop');
 const app = express();
 
 // Templates manager
-app.set('view engine', 'pug');
+// Express-handlebar
+app.engine(
+  'hbs',
+  expressHbs.engine({
+    extname: 'hbs',
+    defaultLayout: 'main-layout',
+    layoutsDir: '',
+  })
+);
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 // Auto Encode requests
@@ -21,7 +31,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404.pug', { pageTitle: 'Page not found' });
+  res.status(404).render('404', { pageTitle: 'Page not found' });
 });
 
 app.listen(3000);

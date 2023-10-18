86fb3de8308e2ffdf9848f38984ff8c449cb3aaf
AlbertKhomich <khomich1022@gmail.com>
2023-10-18 10:55:14 +0200

    pug

--- app.js
+++ app.js
@@ -3,21 +3,25 @@ const path = require('path');
 const bodyParcer = require('body-parser');
 
 const rootDir = require('./util/path');
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
+
+// Templates manager
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
 // Auto Encode requests
 app.use(bodyParcer.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(rootDir, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+  res.status(404).render('404.pug', { pageTitle: 'Page not found' });
 });
 
 app.listen(3000);
